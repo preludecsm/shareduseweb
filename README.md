f5f6c37bf46e7be00d92b033a43796f3fe296653
preludecsm <mick@sentixinc.com>
2013-01-19 11:22:55 -0800

    commit for github

---
+++ README.md
@@ -0,0 +1,4 @@
+# Micktel TF Shared Use Website
+
+Web pages for delivering TF number-specific pages for search or ad placement.
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SharedUseWeb::Application.config.secret_token = '912d1d28dfb42227465c49997207f08f86b883a8540fb6dce233dbae220048e9542958960281c2423b98070575aa8326083a1084ea94d706b155cc3ccb160cd9'
